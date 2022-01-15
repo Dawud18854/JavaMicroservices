0cd90b3caa024258a37a7f25e6cb509e2dedcd49
Dawud Hussein <dawud.hussein@outlook.com>
2022-01-16 00:17:24 +0100

    department service implemented findById and saveDep

--- department-service/src/main/java/at/spengergasse/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/at/spengergasse/departmentservice/service/DepartmentService.java
@@ -2,11 +2,13 @@ package at.spengergasse.departmentservice.service;
 
 import at.spengergasse.departmentservice.domain.Department;
 import at.spengergasse.departmentservice.persistance.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public class DepartmentService {
 
 
@@ -14,7 +16,12 @@ public class DepartmentService {
     private DepartmentRepository departmentRepository;
 
     public Department saveDepartment(Department department) {
-
+        log.info("Inside saveDepartment method of DepartmentService");
         return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside findDepartmentById method of DepartmentService");
+        return departmentRepository.getFindByDepartmentId(departmentId);
     }
 }
