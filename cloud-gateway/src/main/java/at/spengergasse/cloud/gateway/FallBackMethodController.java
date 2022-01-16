3d81433d7e65ad4b7aae5a30af1da601160c8157
Dawud Hussein <dawud.hussein@outlook.com>
2022-01-16 13:34:47 +0100

    started implementing circuit breaker, why is Hystrix no longer supported :(, Mittagspause

---
+++ cloud-gateway/src/main/java/at/spengergasse/cloud/gateway/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package at.spengergasse.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Service seems not to be working,⁄n please try again later" ;
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department Service seems not to be working,⁄n please try again later" ;
+    }
+
+}
